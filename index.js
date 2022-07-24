24f6da8c0b4d03ee8903d438b819aa0327eb7802
kimtahen <kimtahen@naver.com>
2022-07-24 22:49:24 +0900

    error handling logic added

--- index.js
+++ index.js
@@ -6,6 +6,25 @@ app.get('/', (req, res) => {
     res.send('Express Test');
 });
 
+//404 error handling
+app.use(function(req, res, next) {
+  next(); //404 error handling
+});
+
+//global error handling
+app.use(function(err, req, res, next) {
+    /*
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+  */
+    
+});
+
+
 app.listen(port, () => {
     console.log(`Listening...`);
 });
