1efb183df2e3135365ec9c65cce4f4a3806d2847
bbaekddo <bbaekddo100@naver.com>
2022-09-02 15:48:52 +0900

    :wrench: bool-parser 사용

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const compression = require('compression');
 const methodOverride = require('method-override');
 const cors = require('cors');
+const boolParser = require(`express-query-boolean`);
 const {logger} = require("./config/winston");
 
 // process.env 불러오기
@@ -53,6 +54,8 @@ app.use(methodOverride());
 app.use(cookieParser());
 
 app.use(cors(corsOptions));
+
+app.use(boolParser());
 
 app.use(passport.initialize());
 initialize();
