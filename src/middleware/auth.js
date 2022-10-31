0570594469ab4bcda33dd91d64800aa1b996b375
bbaekddo <bbaekddo100@naver.com>
2022-10-31 13:22:05 +0900

    :wrench: authenticate 미들웨어 수정 (토큰 확인 부분)

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -23,8 +23,8 @@ const signUpAuth = async (req, res, next) => {
     // 토큰 복호화
     await jwt.verify(token, process.env.JWT_KEY, async (error, verifiedToken) => {
         if (error) {
-            logger.error(`signUpToken - authenticate middleware error\n${error.message}`);
-            return res.send(errResponse(baseResponse.SIGNUP_TOKEN_EMPTY));
+            logger.error(`signUpToken - signUpAuth middleware error\n${error.message}`);
+            return res.send(errResponse(baseResponse.SIGNUP_TOKEN_VERIFICATION_FAIL));
         }
         
         // request 객체에 토큰 정보 저장
@@ -35,76 +35,32 @@ const signUpAuth = async (req, res, next) => {
 
 // 로그인 토큰 확인
 const authenticate = async (req, res, next) => {
-    // 토큰이 없으면 종료
     let token = null;
     
-    if (req && req.cookies && (req.cookies['accessToken'] !== '')) {
+    token = req.headers['x-access-token'] || req.query.token || req.cookies['accessToken'];
+    /*if (req.cookies !== null && (req.cookies['accessToken'] !== '')) {
         token = req.cookies['accessToken'];
-    }
+    } else {
+        token = req.headers['x-access-token'] || req.query.token;
+    }*/
     
+    // 토큰이 없으면 종료
     if (token === null || token === undefined){
-        next();
-        return;
+        return res.send(errResponse(baseResponse.SIGNIN_TOKEN_EMPTY));
     }
     
     // 토큰 복호화
     await jwt.verify(token, process.env.JWT_KEY, async (error, verifiedToken) => {
         if (error) {
-            res.send(errResponse(baseResponse.ACCESS_TOKEN_EXPIRED));
+            logger.error(`signInToken - authenticate middleware error\n${error.message}`);
+            return res.send(errResponse(baseResponse.SIGNIN_TOKEN_VERIFICATION_FAIL));
         }
-        
-        req.user = verifiedToken;
+    
+        // request 객체에 토큰 정보 저장
+        req.verifiedToken = verifiedToken;
         next();
     });
-   
-    /*try {
-        // 토큰이 없으면 종료
-        let token = null;
-        if (req && req.cookies && (req.cookies['accessToken'] !== '')) {
-            token = req.cookies['accessToken'];
-        }
-        if (token === null || token === undefined){
-            next();
-            return;
-        }
-        
-        // 토큰 복호화
-        let decoded = null;
-        try {
-            decoded = jwt.verify(token, process.env.JWT_KEY);
-        } catch (error) {
-            if (error.name == "tokenExpiredError") {
-                res.send(errResponse(baseResponse.ACCESS_TOKEN_EXPIRED));
-                return;
-            }
-            next();
-            return;
-        }
-        
-        // 토큰 복호화
-        await jwt.verify(token, process.env.JWT_KEY, async (error, verifiedToken) => {
-            if (error) {
-                res.send(errResponse(baseResponse.ACCESS_TOKEN_EXPIRED));
-            }
-            
-            req.user = verifiedToken;
-            next();
-        });
-
-        let user = await prisma.user.findFirst({where : { provider: decoded.provider, email : decoded.email}});
-        if(!user || user.status == "DELETED" || user.status == "STOP"){
-            next();
-            return;
-        }
-
-        req.user = {provider: user.provider, email: user.email, name: user.name, status: user.status};
-        next();
-    } catch (e) {
-        console.log(e);
-        next({ status: 500, message: "internal server error" });
-        
-    }*/
-}
+};
 
 module.exports = {
     signUpAuth,
