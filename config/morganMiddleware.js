f0c85f4067737adcc3d5f771fce67fc7856d9acb
bbaekddo <bbaekddo100@naver.com>
2022-11-21 15:35:33 +0900

    :wrench: morgan 설정 모듈 추가

---
+++ config/morganMiddleware.js
@@ -0,0 +1,23 @@
+const morgan = require('morgan');
+
+// 로깅 형식 설정
+const format = `:remote-addr - :remote-user | ":method :url HTTP/:http-version" | :status | :response-time ms`
+
+// 로깅을 위한 Output Stream 옵션
+const stream = {
+    write: (message) => logger.http(message)
+};
+
+// 로깅 스킵 여부 (400미만 코드면 로깅 안함)
+const skip = (_, res) => {
+    if (process.env.NODE_ENV === 'production') {
+        return res.statusCode < 400;
+    }
+    return false;
+};
+
+const morganLog = morgan(format, { stream, skip });
+
+module.exports = {
+    morganLog
+};
