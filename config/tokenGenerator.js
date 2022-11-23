8d5bf6d454b36585d87c70e0895bf1a7b7a9a1d1
bbaekddo <bbaekddo100@naver.com>
2022-11-23 23:16:20 +0900

    :wrench: 토큰 제너레이터 모듈 추가

--- config/tokenGenerator.js
+++ config/tokenGenerator.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const authService = require('../src/app/Auth/authService');
 const authProvider = require('../src/app/Auth/authProvider');
 
-// 회원가입 JWT
+// 회원가입 토큰
 const signUpToken = async (req, res, userCheckString) => {
     const signUpToken = jwt.sign(
         {
@@ -21,55 +21,41 @@ const signUpToken = async (req, res, userCheckString) => {
     });
 }
 
-// 로그인 JWT
-const loginToken = async (req, res, user) => {
+// 리프레시 토큰
+const refreshToken = async (req, res, user) => {
+    // 리프레시 토큰 발급
+    const refreshToken = jwt.sign(
+        {},
+        process.env.JWT_KEY,
+        {
+            expiresIn: '5d'
+        }
+    );
+    res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        sameSite: 'none',
+        secure  : true,
+        path    : '/auth'
+    });
+    
+    // 기존 세션 정보 불러오기
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    let session;
     try {
-        // 액세스 토큰 발급
-        const accessToken = jwt.sign(
-            {
-                provider: user.provider,
-                email   : user.email
-            },
-            process.env.JWT_KEY,
-            {
-                expiresIn: '3h'
-            }
-        );
-        // 액세스 토큰 쿠키에 저장
-        res.cookie('accessToken', accessToken, {
-            sameSite: 'none',
-            secure  : true,
-            path    : '/',
-            
-        });
-        
-        // 리프레시 토큰 발급
-        const refreshToken = jwt.sign(
-            {},
-            process.env.JWT_KEY,
-            {
-                expiresIn: '5d'
-            }
-        );
-        res.cookie('refreshToken', refreshToken, {
-            httpOnly: true,
-            sameSite: 'none',
-            secure  : true,
-            path    : '/auth'
-        });
-        
-        // 기존 세션 정보 불러오기
-        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-        const session = await authProvider.getSessionByUserId(user.id);
-        
-        // 세션 정보가 있을 경우
+        session = await authProvider.getSessionByUserId(user.id);
+    } catch (error) {
+        throw error;
+    }
+    
+    // 세션 정보 수정/생성
+    try {
         if (session !== null) {
             await authService.updateSession(session.refresh_token, refreshToken);
         } else {
             await authService.createSession(user.id, refreshToken, ip);
         }
     } catch (error) {
-        customLogger.error(`tokenGenerator - database error\n${error.message}`);
+        customLogger.error(`tokenGenerator - update/create session error\n${error.message}`);
         throw error;
     }
 };
@@ -77,5 +63,5 @@ const loginToken = async (req, res, user) => {
 
 module.exports = {
     signUpToken,
-    loginToken,
+    refreshToken,
 }
